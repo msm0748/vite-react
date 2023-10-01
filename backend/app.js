8fa1602b9e937fd11914e00b6a38b79ae34860c1
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-10-02 03:40:17 +0900

    feat: setting server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const cors = require('cors');
 const PORT = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
