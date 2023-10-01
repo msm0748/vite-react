09b9877e999ccfe1e4d7311cc0223b2215e76a98
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-10-02 08:03:41 +0900

    feat: Connection mongodb

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const PORT = 8000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('연결 완료');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`listening on ${process.env.PORT}`);
 });
