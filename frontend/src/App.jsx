f8fa81fa71502caf147208b356063a43d64cae56
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-10-02 09:13:15 +0900

    feat: Add register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './layout';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<MainPage />} />
-        <Route path="login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
--- frontend/src/layout/Header/index.jsx
+++ frontend/src/layout/Header/index.jsx
@@ -10,6 +10,9 @@ function Header() {
         <li>
           <Link to="login">Login</Link>
         </li>
+        <li>
+          <Link to="register">register</Link>
+        </li>
       </ul>
     </header>
   );
