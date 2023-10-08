2664f777ae864fd60c6e072d08c181a99e7b3371
seokmin <msm0748@gmail.com>
2023-10-09 08:03:45 +0900

    feat: Setting react-toastify

--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Layout() {
   return (
     <div className="flex flex-col h-screen justify-between">
+      <ToastContainer
+        position="bottom-right"
+        theme="light"
+        pauseOnHover
+        autoClose={1500}
+      />
       <Header />
       <main className="mb-auto w-10/12 max-w-4xl mx-auto">
         <Outlet />
