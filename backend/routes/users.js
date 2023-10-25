6906c4ad78561caf3fde548a295aef886d8967c2
seokmin <msm0748@gmail.com>
2023-10-25 18:50:01 +0900

    feat: jwt 토큰 생성

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
     return res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    // 존재하는 유저인지 체크
+    if (!user) {
+      return res.status(400).send('Auth failed, email not found');
+    }
+
+    // 비밀번호가 올바른지 체크
+    const isMatch = await user.comparePassword(req.body.password);
+
+    if (!isMatch) {
+      return res.status(400).send('Wrong password');
+    }
+
+    const payload = {
+      userId: user._id.toHexString(),
+    };
+
+    // token을 생성
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    return res.json({ user, accessToken });
   } catch (err) {
     console.log(err);
   }
--- frontend/src/store/thunkFunctions.js
+++ frontend/src/store/thunkFunctions.js
@@ -1,15 +1,21 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axiosInstance from '../utils/axios';
 
-export const registerUser = createAsyncThunk(
-  'user/registerUser',
-  async (body, thunkAPI) => {
-    try {
-      const response = await axiosInstance.post('/users/register', body);
-      return response.data;
-    } catch (err) {
-      console.log(err);
-      return thunkAPI.rejectWithValue(err.response.data || err.message);
-    }
+export const registerUser = createAsyncThunk('user/registerUser', async (body, thunkAPI) => {
+  try {
+    const response = await axiosInstance.post('/users/register', body);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return thunkAPI.rejectWithValue(err.response.data || err.message);
   }
-);
+});
+export const loginUser = createAsyncThunk('user/loginUser', async (body, thunkAPI) => {
+  try {
+    const response = await axiosInstance.post('/users/login', body);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return thunkAPI.rejectWithValue(err.response.data || err.message);
+  }
+});
